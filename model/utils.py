f24e727728ae8c0642eb054090c919965765fbe5
Nho89 <noa1720@gmail.com>
2025-05-20 13:13:11 +0200

    fix: modified paths in utils

--- model/utils.py
+++ model/utils.py
@@ -1,10 +1,11 @@
 import os
 import joblib
 
+base_dir = os.path.dirname(os.path.abspath(__file__))
+gender_map_path = os.path.join(base_dir, "gender_map.pkl")
+gender_map = joblib.load(gender_map_path) 
+
 def map_gender(X):
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    gender_map_path = os.path.join(base_dir, "gender_map.pkl")
-    gender_map = joblib.load(gender_map_path)
     X = X.copy()
     X['gender'] = X['gender'].map(gender_map)
     return X
